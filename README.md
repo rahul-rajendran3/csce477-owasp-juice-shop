3330572d35d9d9c45050b1be25ff23008d8b2dd7
rahul-rajendran3 <117254703+rahul-rajendran3@users.noreply.github.com>
2026-03-22 16:27:29 -0500

    simple frontend login form

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # csce477-owasp-juice-shop
+
+### Simple Front-End Form
+A simple front-end login form using HTML and Vanilla JavaScript, setup with Vite.
+Client side validation:
+- Email must contain '@'
+- Password must be at least 8 characters long
---
+++ frontend/src/main.js
@@ -0,0 +1,39 @@
+import './style.css'
+
+export function validate() {
+  const email = document.getElementById("email").value
+  const password = document.getElementById("password").value
+
+  if (!email.includes("@")) {
+    return false;
+  }
+  if (password.length < 8) {
+    return false;
+  }
+  return true;
+}
+
+document.querySelector('#app').innerHTML = `
+<section id="center">
+  <div>
+    <h1>Juice Shop</h1>
+    <input id="email" type="text" placeholder="email" class="login"/>
+    </br>
+    <input id="password" type="password" placeholder="password" class="login"/>
+  </div>
+  <button id="login-btn" type="button" class="login-btn">log in</button>
+</section>
+`
+document.querySelector('#login-btn').addEventListener('click', () => {
+  const isValid = validate();
+  if (!isValid) {
+    alert("Invalid email or password. Please try again.");
+    return;
+  }
+  else {
+    alert("Login successful!");
+    document.getElementById("email").value = "";
+    document.getElementById("password").value = "";
+    return;
+  }
+});
